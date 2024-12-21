868f6aab347df0c7aeffa7a1fa2eea48e83a8c63
AmanYadav8957 <152906478+AmanYadav8957@users.noreply.github.com>
2024-12-21 23:35:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
-//complete this code
-class Animal {}
+// Animal class definition  
+class Animal {  
+  constructor(species) {  
+    this._species = species; // Initialize species  
+  }  
 
-class Dog extends Animal {}
+  // Getter for species  
+  get species() {  
+    return this._species;  
+  }  
 
-class Cat extends Animal {}
+  // Method to make sound  
+  makeSound() {  
+    console.log(`The ${this.species} makes a sound`);  
+  }  
+}  
 
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
+// Cat class that extends Animal  
+class Cat extends Animal {  
+  constructor(species) {  
+    super(species); // Call the parent constructor  
+  }  
+
+  // Method for purring  
+  purr() {  
+    console.log("purr");  
+  }  
+}  
+
+// Dog class that extends Animal  
+class Dog extends Animal {  
+  constructor(species) {  
+    super(species); // Call the parent constructor  
+  }  
+
+  // Method for barking  
+  bark() {  
+    console.log("woof");  
+  }  
+}  
+
+// Do not change the code below this line  
+window.Animal = Animal;  
+window.Dog = Dog;  
 window.Cat = Cat;
